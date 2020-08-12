7744f27ba07f456d07aa275a813a669ad2f0606b
Kalsang Agongtsang <tznklsng@yahoo.com>
2020-08-12 04:24:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function sayHiToGrandma(string){
   {if (string === string.toLowerCase())
   return "I can't hear you!"; }
   
-  {if (string === string.toUpperCase())
+  {else if (string === string.toUpperCase())
   return "YES INDEED!";}
   
-  {if (string === "I love you,Grandma.")
+  {else (string === "I love you,Grandma.")
   return "I love you,too.";}
   
 }
